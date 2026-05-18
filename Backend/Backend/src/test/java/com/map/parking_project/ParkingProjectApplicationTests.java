fc00c0736c35d802ac399898df7ea41afbad3fe3
Hermesss29 <hermespagu@gmail.com>
2026-05-18 00:04:09 -0500

    fix errores

--- Backend/Backend/src/test/java/com/map/parking_project/ParkingProjectApplicationTests.java
+++ Backend/Backend/src/test/java/com/map/parking_project/ParkingProjectApplicationTests.java
@@ -2,17 +2,16 @@ package com.map.parking_project;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import javax.sql.DataSource;
 
 @SpringBootTest
-@Import(TestMailConfig.class)
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class ParkingProjectApplicationTests {
 
-	@Test
-	void contextLoads() {
-		// Verifica que el contexto de Spring cargue correctamente
-	}
+    @MockBean
+    DataSource dataSource;
 
-
+    @Test
+    void contextLoads() {
+    }
 }
